1f088bc3678f3cfabb77abe67a08272108d8e7d4
Max Novelli <max.novelli@ess.eu>
2023-02-21 15:50:48 +0100

    cleaned up unused functions and variables

--- src/queue/consumers/scicat/scicatProposal/consumerCallbacks/proposalFoldersCreation.ts
+++ src/queue/consumers/scicat/scicatProposal/consumerCallbacks/proposalFoldersCreation.ts
@@ -5,37 +5,6 @@ import fetch from 'node-fetch';
 
 import { ValidProposalMessageData } from '../utils/validateProposalMessage';
 
-const folderCreationCommand = process.env.FOLDER_CREATION_COMMAND;
-
-const getInstrumentName = async (proposalId: string) => {
-  const response = await fetch(process.env.USER_OFFICE_GRAPHQL_URL! as string, {
-    headers: {
-      'content-type': 'application/json',
-      Authorization: 'Bearer ' + process.env.USER_OFFICE_JWT,
-    },
-    body:
-      '{"operationName":null,"variables":{},"query":"{\\n  proposalById(proposalId: \\"' +
-      proposalId +
-      '\\") {\\n    title\\n    instrument {\\n      name\\n    }\\n  }\\n}\\n"}',
-    method: 'POST',
-  });
-
-  try {
-    const {
-      proposalById: { instrument: name },
-    } = await response.json();
-
-    return name as string;
-  } catch (error) {
-    logger.logError('Request for instrument name failed', {
-      error: error,
-      statusCode: response.status,
-      statusText: response.statusText,
-      body: response.body,
-    });
-  }
-};
-
 const proposalFoldersCreation = async (
   proposalMessage: ValidProposalMessageData
 ) => {
