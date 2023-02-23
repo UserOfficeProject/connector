9ee877f660538d8c5510be57ed2c6a7edba2ebcc
Max Novelli <max.novelli@ess.eu>
2023-02-23 13:25:23 +0100

    fixed configuring non default port for rabbitmq

--- src/queue/messageBroker/getRabbitMqMessageBroker.ts
+++ src/queue/messageBroker/getRabbitMqMessageBroker.ts
@@ -15,12 +15,14 @@ const getRabbitMqMessageBroker: GetMessageBroker = async () => {
   await rabbitMq
     .setup({
       hostname: process.env.RABBITMQ_HOSTNAME,
+      port: Number(process.env.RABBITMQ_PORT),
       username: process.env.RABBITMQ_USERNAME,
       password: process.env.RABBITMQ_PASSWORD,
     })
     .then(() => {
       logger.logInfo('Connected to RabbitMQ', {
         hostname: process.env.RABBITMQ_HOSTNAME,
+        port: process.env.RABBITMQ_PORT,
         username: process.env.RABBITMQ_USERNAME,
         password: '********',
       });
