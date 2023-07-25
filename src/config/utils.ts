f406531688077ad919bcaed37dc6f8741bc1abed
root <root@scicatingestor02.daq.esss.dk>
2023-07-25 15:49:26 +0200

    used to change axiom for proxy issues

--- src/config/utils.ts
+++ src/config/utils.ts
@@ -1,3 +1,4 @@
+import axios, { AxiosHeaders, AxiosRequestConfig } from 'axios';
 import { container, Lifecycle } from 'tsyringe';
 
 export const mapClass = (
@@ -26,4 +27,30 @@ export const str2Bool = (i: string): boolean => {
   i = i.toLowerCase();
 
   return i === 'true' || i === '1';
+};
+
+export const axiosFetch = async (
+  url: RequestInfo | URL,
+  options?: RequestInit
+): Promise<Response> => {
+  const { method, headers, body } = options as RequestInit;
+  const axiosOptions = {
+    method,
+    url,
+    data: body,
+    headers,
+  } as AxiosRequestConfig;
+  const axiosResponse = await axios(axiosOptions);
+
+  const { data, status, statusText, headers: axiosHeaders } = axiosResponse;
+
+  const responseHeaders = new AxiosHeaders(axiosHeaders as AxiosHeaders);
+
+  const response = new Response(JSON.stringify(data), {
+    status,
+    statusText,
+    headers: responseHeaders,
+  });
+
+  return response;
 };
