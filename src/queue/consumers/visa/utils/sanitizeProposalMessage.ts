3b97d432caf352d27069b1184fb03d0789a73a70
Yoganandan Pandiyan <p.yoganandan@gmail.com>
2024-10-31 12:57:30 +0100

    orcid lowercasing done

---
+++ src/queue/consumers/visa/utils/sanitizeProposalMessage.ts
@@ -0,0 +1,21 @@
+import { ProposalMessageData } from '../../../../models/ProposalMessage';
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+export function sanitizeProposalMessage(proposalMessage: ProposalMessageData) {
+  return {
+    ...proposalMessage,
+    members: proposalMessage.members.map((member) => ({
+      ...member,
+      oidcSub: member.oidcSub.toLowerCase(),
+      email: member.email.toLowerCase(),
+    })),
+    proposer: proposalMessage.proposer
+      ? {
+          ...proposalMessage.proposer,
+          oidcSub: proposalMessage.proposer.oidcSub.toLowerCase(),
+          email: proposalMessage.proposer.email,
+        }
+      : undefined,
+  };
+}
