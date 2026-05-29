ea06aeb9d1c772c358674e3ac31c5b6e0554f8db
junjiequan <a331998513@gmail.com>
2026-05-29 11:21:46 +0200

    fix test

--- src/queue/consumers/scicat/scicatProposal/mappers/uoToScicatProposal.mapper.spec.ts
+++ src/queue/consumers/scicat/scicatProposal/mappers/uoToScicatProposal.mapper.spec.ts
@@ -10,7 +10,7 @@ const createBaseUoProposal = (
   proposalId: '158548',
   title: 'Test Proposal',
   abstract: 'Test Abstract',
-  status: { id: 1, shortCode: 'ALLOCATED', name: 'ALLOCATED' },
+  status: { id: 1, name: 'ALLOCATED' },
   proposer: {
     id: 1,
     firstname: 'John',
